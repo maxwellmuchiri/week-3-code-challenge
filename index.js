d2dcf090a69a2c443afc3b54f3b2f244ed061a01
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-13 20:45:25 +0300

    declaring values

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
- //Declaring Variables
+//  //Declaring Variables
  
- const films = 'http://localhost:3000/films'
+// //  const films = 'http://localhost:3000/films'
 
- // fetching info from
- const url = 'http://localhost:3000/films'
- function getFilms(){
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {console.log (data)
-    data.forEach (film => {
-        display(film)
-    })
-})
-}
-function display(films){
-    console.log (films.name);
-    const name =document.createElemnt ('p');
-    name.innerHTML = films.name
-    const main = getElementById("main")
-    main.append(buyticket)
-    name.addEventListener ("click", () => )
-}
+// //  // fetching info from
+// //  const url = 'http://localhost:3000/films'
+// //  function getFilms(){
+// //     fetch(url)
+// //     .then(res => res.json())
+// //     .then(data => {console.log (data)
+// //     data.forEach (film => {
+// //         display(film)
+// //     })
+// // })
+// // }
+// // function display(films){
+// //     console.log (films.name);
+// //     const name =document.createElemnt ('p');
+// //     name.innerHTML = films.name
+// //     const main = getElementById("main")
+// //     main.append(buyticket)
+// //     name.addEventListener ("click", () => )
+// // }
+
+
+
+
+
+
+//Declaring values
+
+ const title=document.querySelector(".title");
+ const burrons=document.querySelector(".burrons").addEventListener("click",(x)=>{
+    console.log(x.target)
+ });
+ const poster=documment.querySelector(".poster");
+ const runtime=docuuument.querySelector(".runtime");
+ const tickets=document.querySelector(".ticketsAv");
+
+
